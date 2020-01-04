059b7c8b14f898694c809bc1a770cb5c3aec13a1
manoelsouzadev <manoel.souza280@gmail.com>
2020-01-04 18:51:31 -0300

    Alteracao no .gitignore

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == "production"){
-	module.exports = { mongoURI: "mongodb+srv://manoel:mongodbnosql@cluster0-4lzxg.mongodb.net/test?retryWrites=true&w=majority" }
-}else{
-  module.exports = { mongoURI: "mongodb+srv://manoel:mongodbnosql@cluster0-4lzxg.mongodb.net/test?retryWrites=true&w=majority" }
-}
